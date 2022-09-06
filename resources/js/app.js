7a6e91c99a764126cdbec22445939369cdbbe575
syuumu <syuumu200@gmail.com>
2022-09-06 16:17:23 +0900

    fix

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,12 @@
 require('./bootstrap');
-import { createApp, h } from 'vue'
+import { createSSRApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/inertia-vue3'
 import { InertiaProgress } from '@inertiajs/progress'
 
 createInertiaApp({
   resolve: name => require(`./Pages/${name}`),
   setup({ el, App, props, plugin }) {
-    createApp({ render: () => h(App, props) })
+    createSSRApp({ render: () => h(App, props) })
       .use(plugin)
       .mount(el)
   },
