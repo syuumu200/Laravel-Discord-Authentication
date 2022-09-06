8bbde31b413fd5bf3871e53c95a66528d138bb14
syuumu <syuumu200@gmail.com>
2022-09-07 01:59:22 +0900

    code improvement

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,21 +1,24 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Scripts -->
-        <script src="{{ mix('js/app.js') }}" defer></script>
-        @inertiaHead
-    </head>
-    <body class="antialiased">
-        @inertia
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 
-        @env ('local')
-            <script src="http://localhost:3000/browser-sync/browser-sync-client.js"></script>
-        @endenv
-    </body>
+    <!-- Scripts -->
+    <script src="{{ mix('js/app.js') }}" defer></script>
+    @inertiaHead
+</head>
+
+<body class="antialiased">
+    @inertia
+
+    @env ('local')
+    <script src="http://localhost:3000/browser-sync/browser-sync-client.js"></script>
+    @endenv
+</body>
+
 </html>
