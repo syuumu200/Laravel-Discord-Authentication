7c5297b565b4417e0b0b7be64d8dc1ec9da4e8d5
syuumu <syuumu200@gmail.com>
2022-09-07 01:55:59 +0900

    code improvement

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -1,38 +1,34 @@
 <template>
-<Head :title="$page.props.appName" />
-<div class="max-w-5xl mx-auto sm:px-6 lg:px-8">
-    <h1 class="text-center mt-10 text-4xl mb-4">{{$page.props.appName}}</h1>
+  <Head :title="$page.props.appName" />
+  <div class="max-w-5xl mx-auto sm:px-6 lg:px-8">
+    <h1 class="text-center mt-10 text-4xl mb-4">{{ $page.props.appName }}</h1>
     <div class="text-center">
-        <Link v-if="!$page.props.auth.check" class="underline text-center"
-            href='/login'>Discordでログイン</Link>
-        <div v-else class="text-center">
-            <span>
-                <span class="mr-1">Hi</span>
-                <span class="mr-3">{{$page.props.auth.user.username}} !</span>
-            </span>
-            <Link class="underline" href="/logout">ログアウト</Link>
-        </div>
+      <Link
+        v-if="!$page.props.auth.check"
+        class="underline text-center"
+        href="/login"
+        >Discordでログイン</Link
+      >
+      <div v-else class="text-center">
+        <span>
+          <span class="mr-1">Hi</span>
+          <span class="mr-3">{{ $page.props.auth.user.username }} !</span>
+        </span>
+        <Link class="underline" href="/logout">ログアウト</Link>
+      </div>
     </div>
     <slot></slot>
-</div>
+  </div>
 </template>
 
 <script>
-import {
-    defineComponent
-} from 'vue'
-import {
-    Head,
-    Link
-} from '@inertiajs/inertia-vue3';
+import { defineComponent } from "vue";
+import { Head, Link } from "@inertiajs/inertia-vue3";
 
 export default defineComponent({
-    data() {
-        return {}
-    },
-    components: {
-        Head,
-        Link
-    }
-})
+  components: {
+    Head,
+    Link,
+  },
+});
 </script>
--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,30 +1,19 @@
 <template>
-<AppLayout>
+  <AppLayout>
     <p class="text-center">LaravelとInertiaとDiscord Oauth2を統合しました！</p>
-</AppLayout>
+  </AppLayout>
 </template>
 
-<style scoped>
-</style>
-
 <script>
-import {
-    defineComponent
-} from 'vue'
-import {
-    Head,
-    Link
-} from '@inertiajs/inertia-vue3';
-import AppLayout from '@/Layouts/AppLayout.vue'
+import { defineComponent } from "vue";
+import { Head, Link } from "@inertiajs/inertia-vue3";
+import AppLayout from "@/Layouts/AppLayout.vue";
 
 export default defineComponent({
-    data() {
-        return {}
-    },
-    components: {
-        Head,
-        Link,
-        AppLayout
-    }
-})
+  components: {
+    Head,
+    Link,
+    AppLayout,
+  },
+});
 </script>
