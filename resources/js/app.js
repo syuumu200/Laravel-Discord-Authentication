28a4e474c123744a2796d1f4b4509fbf6bed065d
syuumu <syuumu200@gmail.com>
2023-03-27 10:53:10 +0900

    fix ssr

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,8 @@
 
 import './bootstrap'
 
-import { createApp, h } from 'vue'
+//import { createApp, h } from 'vue'
+import { createSSRApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/vue3'
 import { Link, Head } from '@inertiajs/vue3'
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
@@ -9,7 +10,7 @@ import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 createInertiaApp({
   resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
   setup({ el, App, props, plugin }) {
-    const vue = createApp({ render: () => h(App, props) })
+    const vue = createSSRApp({ render: () => h(App, props) })
     vue.config.globalProperties.$route = route
     vue
       .use(plugin)
